6f06e350f09cf504b1683b61ccef77c9442329b0
imwood04 <41460038+imwood04@users.noreply.github.com>
2020-09-30 04:53:05 -0400

    Upload

--- src/imwood04/Main.php
+++ src/imwood04/Main.php
@@ -1,3 +1,64 @@
 <?php
 
-use
+namespace imwood04;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as TF;
+
+class Main extends PluginBase implements Listener
+{
+
+    public function onLoad()
+    {
+        $this->getLogger()->info("BroadCast Plugin Loading");
+    }
+
+    public function onEnable()
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("BroadCast Enabled!");
+    }
+
+    public function onDisable()
+    {
+        $this->getLogger()->info("BroadCast Disabled");
+    }
+
+    public function onJoin(PlayerJoinEvent $event)
+    {
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        $this->getServer()->broadcastMessage(TF::GREEN . "$name Has Joined the Server!");
+    }
+
+    public function onQuit(PlayerQuitEvent $event)
+    {
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        $this->getServer()->broadcastMessage(TF::RED . "$name Has Quit the Server!");
+
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool
+    {
+        if ($cmd->getName() === "broadcast") {
+            if (!$sender->hasPermission("imwood04.bc")) {
+                $sender->sendMessage(TF::RED . "No Perms!");
+            } else {
+                if (isset($args[0])) {
+                    $msg = implode(" ", $args);
+                    $this->getServer()->broadcastMessage(TF::AQUA . $msg);
+                    $sender->sendMessage(TF::AQUA . "BroadCast" . $msg . "=>");
+                } else {
+                    $sender->sendMessage(TF::RED . "Usage: /broadcast {Your Message}, /bc {Your Message}");
+                }
+            }
+        }
+        return true;
+    }
+}
